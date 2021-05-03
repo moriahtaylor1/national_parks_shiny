3d9a2810e861c3e9cf520ebd3615693bf113e8b0
Moriah Taylor <moriahataylor@gmail.com>
2021-05-02 22:05:13 -0400

    update utility functions

--- util_functions.R
+++ util_functions.R
@@ -20,10 +20,7 @@ park_stats <- read.csv(data_path)
 ###WRANGLING###
 #get rid of x column
 park_stats <- park_stats[,-1]
-#single out parks with missing data in order to condition plot functions
-##no invertebrates
-no_inverts_data <- park_stats %>% filter(count_invert==0)
-no_inverts_list <- no_inverts_data$park_name
+#single out parks with missing data in order to condition output functions
 ##no nonvascular plants
 no_nonvasc_data <- park_stats %>% filter(count_nonvascular==0)
 no_nonvasc_list <- no_nonvasc_data$park_name
@@ -41,8 +38,7 @@ font_add(family = "title2", "fonts/Merriweather-Regular.ttf")
 showtext_auto()
 loadfonts(device = "win")
 #colors
-vert_colors <- c("#AAD213", "#5A1B8A", "#037FD0", "#E68200", "#036D50")
-invert_colors <- c("#92d800", "#fc7400", "#d7004b", "#0372bb", "#d67dd6")
+vert_colors <- c("#AAD213", "#7b49a1", "#037FD0", "#E68200", "#036D50")
 #themes
 plot_theme <- theme(
   # no title, will be labeled on webpage
@@ -52,12 +48,12 @@ plot_theme <- theme(
   panel.grid.major = element_blank(),
   panel.grid.minor = element_blank(),
   panel.background = element_rect(fill = "transparent"),
-  plot.background = element_rect(fill = "#cccccc"),
+  plot.background = element_rect(fill = "#3d4732", color="#1b2313", size=3),
   
   # axis
-  axis.title.y = element_text(family="title", size=20, color="black"),
+  axis.title.y = element_text(family="title", size=20, color="white"),
   axis.text.y = element_blank(),
-  axis.text.x = element_text(family="title2", size=14, color="black"),
+  axis.text.x = element_text(family="title2", size=14, color="white"),
   axis.ticks.x = element_blank(),
   axis.ticks.y = element_blank(),
   
@@ -74,12 +70,12 @@ plot_theme2 <- theme(
   panel.grid.major = element_blank(),
   panel.grid.minor = element_blank(),
   panel.background = element_rect(fill = "transparent"),
-  plot.background = element_rect(fill = "#cccccc"),
+  plot.background = element_rect(fill = "#4e5d6c"),
   
   # axis
-  axis.title.y = element_text(family="title", size=20, color="black"),
+  axis.title.y = element_text(family="title", size=20, color="white"),
   axis.text.y = element_blank(),
-  axis.text.x = element_text(family="title2", size=10, color="black", angle=30),
+  axis.text.x = element_text(family="title2", size=10, color="white", angle=30),
   axis.ticks.x = element_blank(),
   axis.ticks.y = element_blank(),
   
@@ -88,7 +84,7 @@ plot_theme2 <- theme(
 )
 
 
-###PLOTTING FUNCTIONS###
+##PLOTTING FUNCTIONS##
 #VERTEBRATES
 vert_plot <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
@@ -101,7 +97,7 @@ vert_plot <- function(parkName){
   #ggplot
   ggplot(data=vert_data, aes(x=category, y=n, fill=category)) + 
     geom_bar(stat="identity") + ylim(0,plot_max) +
-    geom_text(aes(label=n, family="title2"), vjust=-0.4, size=6, color="black") +
+    geom_text(aes(label=n, family="title2"), vjust=-0.4, size=6, color="white") +
     scale_x_discrete(limits=c("Mammal", "Bird", "Reptile", "Amphibian", "Fish")) +
     scale_fill_manual(values=vert_colors) +
     labs(title="VERTEBRATES") +
@@ -124,73 +120,99 @@ invert_tbl <- function(parkName){
   #create formattable object
   formattable(invert_table,
               align =c("l","r"), 
-              list(`Category` = formatter("span", 
-                                          style = x ~ style(color = "black",font.weight = "bold")),
-                   `Count` = color_bar("#cccccc"), fun = unit.scale))
+              list(area(col=1:2) ~ formatter("span", style = x ~ style(font-size:"30px"))))
 }
 
-
-###RANK FUNCTIONS###
-#mammal rank
-get_rank_m <- function(parkName){
+###GETTING INFO###
+get_park_info <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste0("#", park_data$rank_mammal))
+  park_data$acres <- format(park_data$acres, big.mark=",")
+  park_data$kilos_sq <- format(round(park_data$meters_sq/1000,2), big.mark=",")
+  park_data$kilos_sq <- as.character(park_data$kilos_sq)
+  park_data$latitude <- as.character(park_data$latitude)
+  park_data$longitude <- as.character(park_data$longitude)
+  line1 <- paste0("<big><big><big><big><b>", parkName, "</big></big></big></big></b>")
+  line2 <- paste0("<big><big><big><em>", park_data$state_long, "</big></big></big></em>")
+  line3 <- paste0("<big><big>", park_data$acres, " acres | ", park_data$kilos_sq, " sq kilometers", "</big></big>")
+  line4 <- paste0("<big><big><b>Location: (", park_data$latitude, ", ", park_data$longitude, ")", "</b></big></big>")
+  return(HTML(paste(line1, line2, line3, line4, sep="<br/>")))
 }
 
-#bird rank
-get_rank_b <- function(parkName){
+##ANIMAL HEADING##
+get_animal_header <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste0("#", park_data$rank_bird))
+  park_data$animals_native_percent <- as.character(park_data$animals_native_percent)
+  park_data$animals_endanger <- as.character(park_data$animals_endanger)
+  line1 <- paste0("<big><big><big><big><b>", "ANIMALS", "</big></big></big></big></b>")
+  line2 <- paste0("<big>", park_data$animals_native_percent, "% native species", "</big>")
+  line3 <- paste0("<big>",park_data$animals_endanger, " threatened or endangered species", "</big>")
+  return(HTML(paste(line1, line2, line3, sep="<br/>")))
 }
 
-#reptile rank
-get_rank_r <- function(parkName){
+##GET RANKS##
+get_all_ranks <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste0("#", park_data$rank_reptile))
+  park_data$rank_mammal <- as.character(park_data$rank_mammal)
+  park_data$rank_bird <- as.character(park_data$rank_bird)
+  park_data$rank_reptile <- as.character(park_data$rank_reptile)
+  park_data$rank_amphibian <- as.character(park_data$rank_amphibian)
+  park_data$rank_fish <- as.character(park_data$rank_fish)
+  line1 <- "<h3><b>RANKINGS</b></h3>"
+  line2 <- paste0("<h3 style=color:#E68200>Mammals -- #", park_data$rank_mammal, "</h3>")
+  line3 <- paste0("<h3 style=color:#7b49a1>Birds -- #", park_data$rank_bird, "</h3>")
+  line4 <- paste0("<h3 style=color:#036D50>Reptiles -- #", park_data$rank_reptile, "</h3>")
+  line5 <- paste0("<h3 style=color:#AAD213>Amphibians -- #", park_data$rank_amphibian, "</h3>")
+  line6 <- paste0("<h3 style=color:#037FD0>Fish -- #", park_data$rank_fish, "</h3>")
+  return(HTML(paste(line1, line2, line3, line4, line5, line6, sep="")))
 }
 
-#amphibian rank
-get_rank_a <- function(parkName){
-  park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste0("#", park_data$rank_amphibian))
+##INVERTEBRATES INFO##
+get_invert_info <- function(){
+  line1 <- "<h4 style=color:#93998d><b><em>In case you didn't know...</em></b></h4>"
+  line2 <- "<h4 style=color:#7d8476><em>Arachnids include Spiders and Scorpions</em></h4>"
+  line3 <- "<h4 style=color:#7d8476><em>Gastropods include Snails and Slugs</em></h4>"
+  line4 <- "<h4 style=color:#7d8476><em>Shellfish include Crabs, Lobsters, and Shrimp</em></h4>"
+  return(HTML(paste(line1, line2, line3, line4, sep="")))
 }
 
-#fish_rank
-get_rank_f <- function(parkName){
+##PLANTS NUMBERS##
+get_vasc <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste0("#", park_data$rank_fish))
+  park_data$count_vascular <- as.character(park_data$count_vascular)
+  return(paste("  |", park_data$count_vascular, sep=" "))
 }
 
-##OTHER STATS FUNCTIONS##
-##return strings of stats##
-
-#percentage of native species
-get_nativeness <- function(parkName){
+get_nonvasc <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste0(str(park_data$animals_native_percent), "%"))
+  if (park_data$count_nonvascular == 0){
+    return("| Unknown")
+  }
+  else{
+    park_data$count_nonvascular <- as.character(park_data$count_nonvascular)
+    return(paste("| ", park_data$count_nonvascular, sep=" "))
+  }
 }
 
-#number of endangered or threatened species
-get_endangered <- function(parkName){
+##ALGAE AND FUNGI NUMBERS##
+get_algae <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
-  return(str(park_data$animals_endanger))
+  if (park_data$count_algae == 0){
+    return("Unknown |")
+  }
+  else{
+    park_data$count_algae <- as.character(park_data$count_algae)
+    return(paste(park_data$count_algae, " |", sep=""))
+  }
 }
 
-###GETTING TEXT###
-#state
-get_state <- function(parkName){
+get_fungi <- function(parkName){
   park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste(park_data$state_long))
+  if (park_data$count_fungi == 0){
+    return("Unknown  |")
+  }
+  else{
+    park_data$count_fungi <- as.character(park_data$count_fungi)
+    return(paste(park_data$count_fungi, " |", sep=""))
+  }
 }
-#area
-get_area <- function(parkName){
-  park_data <- park_stats %>% filter(park_name==parkName)
-  park_data$acres <- as.character(park_data$acres)
-  park_data$meters_sq <- as.character(park_data$meters_sq)
-  return(paste(park_data$acres, " acres | ", park_data$meters_sq, " sq meters", sep=""))
-}
-#location
-get_loc <- function(parkName){
-  park_data <- park_stats %>% filter(park_name==parkName)
-  return(paste("Location: (", park_data$latitude, ", ", park_data$longitude, ")", sep=""))
-}
+
